377cc87f4bfe620cd66b7993735a2e794cf0e4b4
Joluife <joluife.26@gmail.com>
2023-08-21 10:53:55 -0400

    se añaden validaciones beforeupdate y beforeinsert

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { SubscriptionLoggable } from "rxjs/internal/testing/SubscriptionLoggable";
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate } from "typeorm";
 
 @Entity()
 export class Product {
@@ -10,4 +11,55 @@ export class Product {
         unique: true,
     })
     title: string
+
+    @Column('float', {
+        default: 0
+    })
+    price: number
+
+    @Column({
+        type: 'text',
+        nullable: true,
+    })
+    description: string
+
+    @Column('text',{
+        unique: true,
+    })
+    slug: string
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number
+
+    @Column('text', {
+        array: true
+    })
+    sizes: string[]
+
+    @Column('text')
+    gender: string
+
+    @BeforeInsert()
+    checkSlugInsert(){
+
+        if(!this.slug) {
+            this.slug = this.title
+        }
+        this.slug = this.slug
+            .toLocaleLowerCase()
+            .replaceAll(' ','_')
+            .replaceAll("'","")
+
+    }
+
+    @BeforeUpdate()
+    checkSlugUpdate(){
+        
+        this.slug = this.slug
+            .toLowerCase()
+            .replaceAll(' ','_')
+            .replaceAll("'","")
+    }
 }
