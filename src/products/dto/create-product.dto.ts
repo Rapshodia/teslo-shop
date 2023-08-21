fc1ece8ec791efb42fd8e8a8783b81be97ff231c
Joluife <joluife.26@gmail.com>
2023-08-21 10:50:54 -0400

    creamos los dto del producto con respectivas validaciones

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,33 @@
-export class CreateProductDto {}
+import {IsString, MinLength, IsPositive, IsNumber, IsOptional, IsInt, IsArray, IsIn} from 'class-validator'
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?:string
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+
+    
+    @IsArray()
+    sizes: string[]
+
+    @IsIn(['men', 'women', 'kids'])
+    gender: string;
+}
